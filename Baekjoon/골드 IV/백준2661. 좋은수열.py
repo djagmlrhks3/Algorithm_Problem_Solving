81668dc0e2fab096abb16377e22bb02d7262e815
djagmlrhks3 <djagmlrhks3@naver.com>
2021-02-08 20:38:49 +0900

    0208(월) | BaekJoon | 2661. 좋은 수열

---
+++ Baekjoon/골드 IV/백준2661. 좋은수열.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('백준2661. 좋은수열.txt', 'r')
+
+def check(num):
+    length = len(num)
+    for idx in range(1, length//2 + 1):
+        if num[-idx:] == num[-(idx*2):-idx]:
+            return False
+    else:
+        return True
+
+def recursive(num):
+    global N, res
+    if len(num) == N:
+        print(num)
+        exit()
+    for i in '123':
+        if check(num + str(i)):
+            recursive(num + str(i))
+    return
+
+N = int(input())
+recursive('1')
